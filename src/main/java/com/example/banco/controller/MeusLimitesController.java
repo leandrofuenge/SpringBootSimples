22e252153a84c40c9cb867f31317855513a4b3ea
Leandro Costa Magalhães Silva <leandrodevbackend@gmail.com>
2024-04-12 22:23:25 -0400

    update

--- src/main/java/com/example/banco/controller/MeusLimitesController.java
+++ src/main/java/com/example/banco/controller/MeusLimitesController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/meus limites")
+@RequestMapping("api/meus-limites")
 public class MeusLimitesController {
 
     private final MeusLimitesService meusLimitesService;
